88f058e499ceb6661fec3070a06c2d84b67167ec
unknown <manatharv@gmail.com>
2021-08-10 14:41:26 +0530

    saved

---
+++ MyFirst/src/test/java/MyTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyTest {
+	
+	
+	@Test
+	
+	public void myTest() {
+		assertEquals("Hello", "Hello");
+	}
+
+}
